94c8e5bd94456cd0f8ac5965088b301c9313c19f
Anna Ji <ji.a@northeastern.edu>
2023-12-04 22:28:36 -0500

    a6 backend

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-import express from 'express';
+import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Courses/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
 import ModuleRoutes from "./Modules/routes.js";
+import session from "express-session";
 import "dotenv/config";
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: process.env.FRONTEND_URL,
+}));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
